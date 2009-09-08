a5b75ee6d7e949fb3becf763939d02b7967c93fc
jopeek_cp <SND\jopeek_cp>
2009-09-08 01:59:03 +0000

    test

---
+++ Browser Chooser/Browser.vb
@@ -0,0 +1,33 @@
+﻿Public Class Browser
+    Private nameValue As String
+    Public Property Name() As String
+        Get
+            Return nameValue
+        End Get
+        Set(ByVal value As String)
+            nameValue = value
+        End Set
+    End Property
+    Private pathValue As String
+    Public Property Path() As String
+        Get
+            Return pathValue
+        End Get
+        Set(ByVal value As String)
+            pathValue = value
+        End Set
+    End Property
+
+
+    Public Sub New()
+
+    End Sub
+    Public Sub New(ByVal Name As String, ByVal Path As String)
+        MyClass.nameValue = Name
+        MyClass.pathValue = Path
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Name
+    End Function
+End Class
---
+++ Browser Chooser/download.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Download
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Download))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(784, 562)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("http://browserchooser.codeplex.com/Release/ProjectReleases.aspx#ReleaseFiles", System.UriKind.Absolute)
+        '
+        'Download
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Download"
+        Me.Text = "Download Browser Chooser Update"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+End Class
